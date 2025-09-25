8787929909ef2704541df5b2a123745a8ce1a5ae
rangu-rahul <your-email@example.com>
2025-09-25 11:12:17 +0530

    variables

--- python.py
+++ python.py
@@ -1,5 +1,70 @@
-import sys
-print(sys.version)
+# import sys
+# print(sys.version)
+
+#simple print statement
+# print("Hello World!")
+
+#printing two variables in a single line
+# a = 789
+# b = 123
+# print(a, b)
+# print("a :{},b : {}".format(a,b))
+
+#two variable one line
+# a = b = 10
+# print(a,b)
+# print(a *2)
 
 
-print("Hello World!")
+#unknow two variables assign
+# a, b = 10, 20
+# c, d = a, b
+# print(c,d,a,b)
+
+
+#assign 3 variables with name of x,y,z = 56,90,34
+# x, y, z = 56, 90, 34
+# print(x, y, z)
+# print(x*y, y*z, z*x) 
+
+#types of variables
+# name = "Rahul"
+# age = 25
+# phone = "91XXX53961"
+# student = True
+# print(type(name), type(age), type(phone), type(student))
+
+#hours in a week
+# days = 7
+# one_day = 24
+# hours = days * one_day
+# minutes = hours * 60
+# seconds = minutes * 60
+# print("hours:", hours)
+# print("minutes:", minutes)
+# #print("seconds:", seconds, end ="\n" , seconds)
+
+
+#input from user and convert into minutes
+# count = input("Enter a number: ")
+# min = 60
+# print("Your minutes is :", int(count) * min)
+
+
+
+# #age into days, hours, minutes, seconds
+# countt = input("Enter a age: ")
+# days = int(countt) * 365
+# hour = days * 24
+# minutes = hour * 60
+# seconds = minutes * 60
+# print("Your days is :", days)
+# print("Your hour is :", hour)
+# print("Your minutes is :", minutes) 
+# print("Your seconds is :", seconds)
+
+
+
+#tuple
+tuple = (1, 2, 3, 4, 5)
+print(tuple)
